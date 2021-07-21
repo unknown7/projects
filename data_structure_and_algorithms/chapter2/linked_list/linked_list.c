2c072957327e94f85685eb154b30821a27bc843f
unknown7 <unknown7@live.cn>
2021-07-21 15:18:26 +0800

    ds

--- data_structure_and_algorithms/chapter2/linked_list/linked_list.c
+++ data_structure_and_algorithms/chapter2/linked_list/linked_list.c
@@ -9,6 +9,8 @@ typedef struct Node {
 Linked_List init(void);
 void head_insert(Linked_List);
 void tail_insert(Linked_List);
+Linked_List locate_index(Linked_List, int);
+void print_list(Linked_List);
 
 Linked_List init() {
 	Linked_List head = malloc(sizeof(struct Node));
@@ -50,6 +52,18 @@ void tail_insert(Linked_List head) {
 	}
 }
 
+Linked_List locate_index(Linked_List head, int index) {
+	if (head == NULL || index < 1 || index > head->data)
+		return NULL;
+
+	Linked_List node = head->next;
+	int i = 1;
+	while (i++ < index)
+		node = node->next;
+
+	return node;
+}
+
 void print_list(Linked_List head) {
 	Linked_List node = head;
 	while (node != NULL) {
@@ -66,6 +80,12 @@ int main() {
 	print_list(list);
 	tail_insert(list);
 	print_list(list);
+
+	int locate_index;
+	printf("locate:");
+	scanf("%d", &locate_index);
+	Linked_List node = locate_index(list, locate_index);
+	printf("locate index:%d=%d\n", locate_index, node->data);
 	return 0;
 }
 
